544589a5866b335219bf2e127c6c9dddb2ced69f
camiegray <camieg4@gmail.com>
2025-03-04 12:00:58 -0800

    update

--- seb224/labs/javascript-browser-game-tamagotchi-lab/js/app.js
+++ seb224/labs/javascript-browser-game-tamagotchi-lab/js/app.js
@@ -2,37 +2,46 @@
 
 // 2) Store cached element references.
 
-// 3) Upon loading, the game state should be initialized, and a function should 
+// 3) Upon loading, the game state should be initialized, and a function should
 //    be called to render this game state.
 
 // 4) The state of the game should be rendered to the user.
 
-// 5) Handle the game over logic. 
+// 5) Handle the game over logic.
 
-// 6) Handle each instance of a player clicking a button with the use of a 
+// 6) Handle each instance of a player clicking a button with the use of a
 //    `handleClick()` function.
 
 // 7) Create reset functionality.
 
-
 let state = {
-    boredom: 0,
-    hunger: 0,
-    sleepiness: 0
-}
- 
-let timer 
+  boredom: 0,
+  hunger: 0,
+  sleepiness: 0,
+};
 
-let gameover
+// let timer
 
+// let gameover
+// let clickFunction = function {}
 
-const boredomBtn = document.createElement("boredom-btn").addEventListener("click")
-const hungerBtn = document.createElement("hunger-btn").addEventListener("click")
-const sleepinessBtn = document.createElement("sleepiness-btn").addEventListener("click")
+const boredomStat = state.boredom;
+const hungerStat = state.hunger;
+const sleepinessStat = state.sleepiness;
+console.log(boredomElement);
 
-const playBtn = document.createElement("play-btn").addEventListener("click")
-const sleepBtn = document.createElement("sleep-btn").addEventListener("click")
-const feedBtn = document.createElement("feed-btn").addEventListener("click")
+const playBtn = document .querySelector("#play") .addEventListener("click", function (event) {
+    console.log("adsfsf");
+  });
+const sleepBtn = document.querySelector("#sleep").addEventListener("click", function (event) {
+    console.log("haha that tickles");
+  });
 
-const resetBtn = document.createElement("reset-btn").addEventListener("click")
-const gameMesage = document.createElement("game-message").addEventListener("click")
+const feedBtn = document .querySelector("#feed") .addEventListener("click", function (event) {
+    console.log("rrrr");
+  });
+
+// const resetBtn = document.querySelector("#reset-btn").addEventListener("click", clickFunction) {
+//     console.log()})
+// const gameMesage = document.querySelector("game-message").addEventListener("click", clickFunction) {
+//     console.log()})
