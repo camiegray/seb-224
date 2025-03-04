8e9337f4fe6dfb2908305573e915243edc44d98a
camiegray <camieg4@gmail.com>
2025-03-04 13:34:44 -0800

    update

--- seb224/labs/dom-events-lab/app.js
+++ seb224/labs/dom-events-lab/app.js
@@ -1,26 +1,33 @@
 
-// As a user, I want to be able to select numbers so that I can perform operations with them.
-// As a user, I want to be able to add two numbers together.
-// As a user, I want to be able to subtract one number from another.
-// As a user, I want to be able to multiply two numbers together.
-// As a user, I want to be able to divide one number by another.
-// As a user, I want to be able to see the output of the mathematical operation.
-// As a user, I want to be able to clear all operations and start from 0.
+const display = document.querySelector(".display");
 
-
-
-
-
-
-console.log("wtf")
-
-const buttons = document.querySelectorAll(".button.number"); 
-buttons.forEach(button => {
+const buttons = document.querySelectorAll(".button.number");
+buttons.forEach((button) => {
   button.addEventListener("click", function (event) {
-    console.log("rrar");
+    display.textContent += button.textContent;
   });
 });
 
+const operators = document.querySelectorAll(".button.operator");
+operators.forEach((operator) => {
+  operator.addEventListener("click", function (event) {
+    display.textContent += operator.textContent;
+  });
+});
+
+const clearButton = document .querySelector(".button.clear") .addEventListener("click", function (event) {
+    display.textContent = "";
+  });
+
+const equalsButton = document.querySelector(".button.equals") .addEventListener("click", function (event) {
+    const expression = display.textContent;
+    if (/[0-9+\-*/]*$/.test(expression)) {
+      display.textContent = eval(expression);
+    } else {
+      display.textContent = "Error";
+ } });
+
+  
 // const calculator = document.querySelector("#calculator").addEventListener ("click", funciton(event) {
 //     console.log(event.target.innerText)
 // )}
