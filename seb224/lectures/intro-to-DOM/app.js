411f2da8b60ed0abc172ca81ac49940c51461ee1
camiegray <camieg4@gmail.com>
2025-03-04 07:34:54 -0800

    update

--- seb224/lectures/intro-to-DOM/app.js
+++ seb224/lectures/intro-to-DOM/app.js
@@ -6,4 +6,4 @@ console.log(titleElement)
 const paragraphElement = document.querySelector (".cool")
 console.log(paragraphElement)
 
-titleElement.textContent = "Advanced DOM Techniques\"65trfc =p-
+titleElement.textContent = "Advanced DOM Techniques"
