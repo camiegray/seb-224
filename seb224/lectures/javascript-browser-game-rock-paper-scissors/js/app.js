d840426ee4e66b990224cc10330920bab381b0a1
camiegray <camieg4@gmail.com>
2025-03-06 06:58:02 -0800

    update

--- seb224/lectures/javascript-browser-game-rock-paper-scissors/js/app.js
+++ seb224/lectures/javascript-browser-game-rock-paper-scissors/js/app.js
@@ -1,1 +1,24 @@
 console.log("sup")
+
+
+
+/*-------------------------------- Constants --------------------------------*/
+
+const choices = ['rock', 'paper', 'scissors'];
+/*-------------------------------- Variables --------------------------------*/
+
+let playerChoice;
+let computerChoice; 
+let msg;
+
+/*------------------------ Cached Element References ------------------------*/
+
+const resultDisplayEl = document.querySelector('#result-display');
+
+/*-------------------------------- Functions --------------------------------*/
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+document.querySelector('#rock').addEventListener('click', play);
+document.querySelector('#paper').addEventListener('click', play);
+document.querySelector('#scissors').addEventListener('click', play);
