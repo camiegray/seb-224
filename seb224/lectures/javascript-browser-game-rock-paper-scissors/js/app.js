f9026c4c8825a15b8c31871abf8cfa2c132a75fd
camiegray <camieg4@gmail.com>
2025-03-06 07:37:54 -0800

    update

--- seb224/lectures/javascript-browser-game-rock-paper-scissors/js/app.js
+++ seb224/lectures/javascript-browser-game-rock-paper-scissors/js/app.js
@@ -1,5 +1,19 @@
 console.log("sup")
 
+// 1) Define any variables used to track the state of the game:
+//    The players choice
+//    The computers choice
+//    The match result - win/lose/tie
+//    A result message - display who won that round
+
+// 2) Define the required constants:
+//    There are only 3 choices a user can make ("rock", "paper", "scissors")
+//    We'll need a reference to a DOM element to display messages
+
+// 3) Handle a player clicking a button
+
+// 4) Handle generating random selections for the computer player
+
 
 
 /*-------------------------------- Constants --------------------------------*/
@@ -22,3 +36,4 @@ const resultDisplayEl = document.querySelector('#result-display');
 document.querySelector('#rock').addEventListener('click', play);
 document.querySelector('#paper').addEventListener('click', play);
 document.querySelector('#scissors').addEventListener('click', play);
+
