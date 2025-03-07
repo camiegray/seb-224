ecf6bedef9ca208df8e256dac0dd633b8ce7ab81
camiegray <camieg4@gmail.com>
2025-03-07 08:19:39 -0800

    update

--- seb224/lectures/javascript-array-iterator-methods/app.js
+++ seb224/lectures/javascript-array-iterator-methods/app.js
@@ -30,17 +30,101 @@
 // nums.forEach((num)=>{
 //     squared.push(n^=2)
 //     console.log(squared)
-const nums = [1, 2, 3, 5, 10]
-const evens = []
-nums.map((num) => {
-    if (num % 2 === 0) {
-        evens.push(num)
-    }
-})
-console.log(evens)
+// const nums = [1, 2, 3, 5, 10]
+// // const evens = []
+// // nums.map((num) => {
+// //     return num % 2 === 0
+        
+    
+// // })
+// // console.log(evens)
 
 
-const squared = nums.map((num)=>{
-    return num^2
-})
-console.log(squared)
+// const squared = nums.map((num) => {
+//     return num**2
+// })
+// console.log(squared)
+
+// const instructors = ['Beryl', 'Hunter', 'Joe', 'Jurgen', 'Ben', 'David']
+// const greatness = instructors.map((greats) => {
+//     return  `${greats} is awesome`
+// })
+// console.log(greatness)
+
+
+
+
+// const numbers = [100, 2, 5, 42, 99, 200, 7, 120]
+// const numsOver50 = numbers.filter((number) => {
+//     return number > 50
+
+// })
+
+
+
+// console.log(numsOver50)
+
+
+
+// const people = ['jerks', 'nice people', 'jerks', 'nice people', 'nice people'];
+//  const notJerks = people.filter((person) => {
+//     return person !== 'jerks'
+
+
+
+//  })
+//  console.log(notJerks)
+
+
+ const cars = [
+    {color: 'red', make: 'BMW', year: 2001},
+    {color: 'white', make: 'Toyota', year: 2013},
+    {color: 'blue', make: 'Ford', year: 2014},
+    {color: 'white', make: 'Tesla', year: 2016}
+  ];
+  
+//   const firstWhiteCar = cars.find((car) => {
+//     return car.color === 'white';
+//   })
+//   // firstWhiteCar is {color: 'white', make: 'Toyota', year: 2013}
+//   console.log(firstWhiteCar)
+//   const missingCar = cars.find((car) => {
+//     return car.color === 'black';
+//   });
+//   console.log(missingCar)
+//   // missingCar = undefined
+
+
+
+
+
+const blueIndex = cars.findIndex((car) => {
+    return car.color === "blue"
+   })
+   console.log(blueIndex)
+//   const missingCarIdx = cars.findIndex((car) => {
+//     return car.color === 'black';
+//   });
+//   // missingCarIdx = -1
+const blueInd = cars.some((car) => {
+    return car.color === "blue"
+   })
+   console.log(blueInd)
+
+   const blueIdx = cars.((car) => {
+    return car.color === "blue"
+   })
+   console.log(blueIdx)
+
+
+
+
+   const thingsInMyRoom = [
+    'random elephant', 
+    'random elephant', 
+    'random elephant', 
+    'random elephant'
+  ];
+  
+  const isEverythingInMyRoomARandomElephant = /* Fill code in here */
+  
