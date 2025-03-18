bfe1076e3dda58d633a1060fe939791fdaa92e3d
camiegray <camieg4@gmail.com>
2025-03-18 11:13:22 -0700

    Create new server files

--- seb224/lectures/intro-to-express/server.js
+++ seb224/lectures/intro-to-express/server.js
@@ -1,9 +1,11 @@
 // Import Express
-import express, { request } from "express"
+import express from "express"
+import morgan from "morgan"
+
 
 // Create an Express app
 const app = express();
-
+app.use(morgan("dev"))
 
 // Define routes here (we'll add them soon)
 
@@ -24,6 +26,13 @@ app.get("/:id", (req, res) => {
   console.log(req.params)
   res.send(`<h1>id: ${req.params.id}</h1>`);
 });
+
+
+app.get("/greet/:name", (req, res) => {
+  const {name} = req.params
+  res.send(`<h1> greetings ${req.params.name} </h1>`)
+})
+
 // Listen for requests on port 3000
 app.listen(3000, () => {
   console.log("Express app is running on port 3000");
