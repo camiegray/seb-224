1c1d278e690821f41a2a7c0a4c514dda1f187ed9
camiegray <camieg4@gmail.com>
2025-03-20 11:12:20 -0700

    Add async file

---
+++ seb224/lectures/intro-to-asynchronous-programming/app.js
@@ -0,0 +1,12 @@
+const fs = require('node:fs');
+
+fs.readFile('test.txt', 'utf8', (err, data) => { 
+  console.log(data);
+});
+const fs = require('node:fs');
+
+fs.readFile('test.txt', 'utf8', (err, data) => { 
+  console.log(data);
+});
+
+console.log('Run this as soon as possible.');
