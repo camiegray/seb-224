60f3e34574ccc19b082160116205eba52f0c2976
camiegray <camieg4@gmail.com>
2025-03-20 11:32:42 -0700

    Add test txt files

--- seb224/lectures/intro-to-asynchronous-programming/app.js
+++ seb224/lectures/intro-to-asynchronous-programming/app.js
@@ -1,12 +1,17 @@
+
+
 const fs = require('node:fs');
 
-fs.readFile('test.txt', 'utf8', (err, data) => { 
+fs.readFile('test.txt', 'utf8', (err, data) => {
   console.log(data);
+  fs.readFile('test2.txt', 'utf8', (err2, data2) => {
+    console.log(data2);
+    fs.readFile('test3.txt', 'utf8', (err3, data3) => {
+      console.log(data3);
+    });
+  });
 });
-const fs = require('node:fs');
 
-fs.readFile('test.txt', 'utf8', (err, data) => { 
-  console.log(data);
-});
+
 
 console.log('Run this as soon as possible.');
