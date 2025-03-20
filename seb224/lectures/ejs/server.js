12c20cf1e7ff5a5069b1689894053573de56b696
camiegray <camieg4@gmail.com>
2025-03-20 10:38:29 -0700

    Update info with  routes

--- seb224/lectures/ejs/server.js
+++ seb224/lectures/ejs/server.js
@@ -1,10 +1,30 @@
 import express from "express"
 const app = express();
 
+const inventory = [
+    { name: 'Candle', qty: 4 },
+    { name: 'Cheese', qty: 10 },
+    { name: 'Phone', qty: 1 },
+    { name: 'Tent', qty: 0 },
+    { name: 'Torch', qty: 5 }
+  ]
+
 app.get('/', (req, res) => {
     console.log("bananas")
-  res.send('The server is running!');
+  res.render('home.ejs', {
+    msg: "Welcome tinfoil",
+    player: {
+        name: "crazies",
+    },
+    inventory: inventory,
+    });
 });
+
+app.get("/:itemID", (req, res)=> {
+console.log(req);
+
+})
+
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
