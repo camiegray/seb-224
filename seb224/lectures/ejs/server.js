163ca2177979446fe6ace95e562489b604198c5c
camiegray <camieg4@gmail.com>
2025-03-20 07:57:09 -0700

    Add home.ejs

---
+++ seb224/lectures/ejs/server.js
@@ -0,0 +1,11 @@
+import express from "express"
+const app = express();
+
+app.get('/', (req, res) => {
+    console.log("bananas")
+  res.send('The server is running!');
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
