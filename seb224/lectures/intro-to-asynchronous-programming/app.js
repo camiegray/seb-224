3c09259b4a171e7c2a07541ed20354448cb10a2b
camiegray <camieg4@gmail.com>
2025-03-20 12:13:46 -0700

    update

--- seb224/lectures/intro-to-asynchronous-programming/app.js
+++ seb224/lectures/intro-to-asynchronous-programming/app.js
@@ -1,17 +1,28 @@
+const fs = require('node:fs/promises');
 
-
-const fs = require('node:fs');
-
-fs.readFile('test.txt', 'utf8', (err, data) => {
+const readDataFiles = async () => {
+  const data = await fs.readFile('test.txt', 'utf8');
   console.log(data);
-  fs.readFile('test2.txt', 'utf8', (err2, data2) => {
-    console.log(data2);
-    fs.readFile('test3.txt', 'utf8', (err3, data3) => {
-      console.log(data3);
-    });
-  });
-});
+  const data2 = await fs.readFile('test2.txt', 'utf8');
+  console.log(data2);
+  const data3 = await fs.readFile('test3.txt', 'utf8');
+  console.log(data3);
+}
 
 
+readDataFiles();
 
-console.log('Run this as soon as possible.');
+const readAnotherFile = async () => {
+  try {
+    // asynchronous operation reading a file that doesn't exist
+    const data = await fs.readFile('test4.txt', 'utf8');
+    console.log(data);
+  } catch (error) {
+    // console logging the error lets us see what went wrong
+    console.log(error);
+  }
+}
+
+readAnotherFile();
+
+console.log('run this as soon as possible');
