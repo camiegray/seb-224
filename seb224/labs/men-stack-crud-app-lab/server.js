a20fca74798ae6a4d711f60c6cfc5f85d146fa47
camiegray <camieg4@gmail.com>
2025-03-25 11:58:30 -0700

    Add new view pages

--- seb224/labs/men-stack-crud-app-lab/server.js
+++ seb224/labs/men-stack-crud-app-lab/server.js
@@ -1,5 +1,40 @@
-import express from "express"
-import mongoose from "mongoose"
-import ejs from "ejs"
-import express from "express"
+import express from "express";
+import "./db/connections.js";
+import methodOverride from "method-override";
+import morgan from "morgan";
 import { configDotenv } from "dotenv";
+configDotenv();
+
+
+const app = express();
+
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"))
+app.use(morgan("dev"))
+app.set("view engine", "ejs");
+
+// Routes
+app.get("/", (req, res) => {
+    res.render("home");
+  });
+  
+app.get("/planets", (req, res) => {
+    res.render("planets/landing.ejs");
+  });
+app.get("/add", (req, res) => {
+    res.render("views/add");
+  });
+app.get("/gas/:id", (req, res)=>{
+
+
+})
+app.get("/rocky/:id", (req, res)=> {
+    const {id} = req.params;
+    const planet = 
+res.render
+    
+})
+app.listen(3000, () => {
+  console.log("App is running on port: 3000");
+})
