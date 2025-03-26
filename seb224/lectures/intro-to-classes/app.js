36d5a83a95f422e232a507f77f952500a730b066
camiegray <camieg4@gmail.com>
2025-03-26 07:47:54 -0700

    Commit

---
+++ seb224/lectures/intro-to-classes/app.js
@@ -0,0 +1,11 @@
+const myFirstCar = {
+    make: 'Toyota',
+    model: 'Corolla',
+    color: 'black',
+    isRunning: false,
+    start: function() {
+      myFirstCar.isRunning = true;
+      console.log('Running!');
+    },
+  };
+  
