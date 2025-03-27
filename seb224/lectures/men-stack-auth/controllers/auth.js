c8e21f8a871c75b1c485aaedb25f3a64a2f90e71
camiegray <camieg4@gmail.com>
2025-03-27 11:10:51 -0700

    Update

--- seb224/lectures/men-stack-auth/controllers/auth.js
+++ seb224/lectures/men-stack-auth/controllers/auth.js
@@ -1,66 +1,82 @@
 import { Router } from "express";
-const router = Router();
+import isSignedIn from "../middleware/is-signed-in.js";
 import User from "../models/user.js";
 import bcrypt from "bcrypt";
 
+const router = Router();
+
+// list our routes
 router.get("/sign-up", (req, res) => {
-  res.render("auth/sign-up.ejs");
+  res.render("auth/sign-up");
+});
+
+router.get("/sign-in", (req, res) => {
+  res.render("auth/sign-in");
 });
 
 router.post("/sign-up", async (req, res) => {
+  // Check if password and confirm password match
   if (req.body.password !== req.body.confirmPassword) {
     return res.send("Password and Confirm Password must match");
   }
 
-  const userInDatabase = await User.findOne({ username: req.body.username });
-  if (userInDatabase) {
+  // Check if username already in database
+  const userInDB = await User.findOne({ username: req.body.username });
+  if (userInDB) {
     return res.send("Username already taken.");
   }
+
+  // Hash our plain-text password before saving to DB (for security)
   const hashedPassword = bcrypt.hashSync(req.body.password, 10);
-  // validation logic
 
   const user = await User.create({
     username: req.body.username,
     password: hashedPassword,
   });
 
-  res.send(`Thanks for signing up ${user.username}`);
-});
+  req.session.user = {
+    _id: user._id,
+    username: user.username,
+  };
 
-router.get("/sign-in", (req, res) => {
-  res.render("auth/sign-in.ejs");
+  req.session.save(() => {
+    res.redirect("/");
+  });
 });
 
 router.post("/sign-in", async (req, res) => {
-    // First, get the user from the database
-    const userInDatabase = await User.findOne({ username: req.body.username });
-    if (!userInDatabase) {
-      return res.send("Login failed. Please try again.");
-    }
-  
-    // There is a user! Time to test their password with bcrypt
-    const validPassword = bcrypt.compareSync(
-      req.body.password,
-      userInDatabase.password
-    );
-    if (!validPassword) {
-      return res.send("Login failed. Please try again.");
-    }
-  
-    // There is a user AND they had the correct password. Time to make a session!
-    // Avoid storing the password, even in hashed format, in the session
-    // If there is other data you want to save to `req.session.user`, do so here!
-    req.session.user = {
-      username: userInDatabase.username,
-      _id: userInDatabase._id,
-    };
-  
+  const userInDatabase = await User.findOne({ username: req.body.username });
+  if (!userInDatabase) {
+    return res.send("Login failed. Please try again.");
+  }
+
+  const validPassword = bcrypt.compareSync(
+    req.body.password,
+    userInDatabase.password
+  );
+
+  if (!validPassword) {
+    return res.send("Login failed. Please try again.");
+  }
+
+  req.session.user = {
+    _id: userInDatabase._id,
+    username: userInDatabase.username,
+  };
+
+  req.session.save(() => {
     res.redirect("/");
   });
-  
-  router.get("/sign-out", (req, res) => {
-    req.session.destroy();
+});
+
+router.get("/sign-out", (req, res) => {
+  req.session.destroy(() => {
     res.redirect("/");
   });
-  
+});
+
+router.get("/vip", isSignedIn, (req, res) => {
+  res.render("auth/vip-lounge");
+});
+
 export default router;
--- seb224/lectures/men-stack-auth/server.js
+++ seb224/lectures/men-stack-auth/server.js
@@ -1,61 +1,51 @@
-import mongoose from "mongoose"
-import "dotenv/config"
-import express from "express"
-import ejs from "ejs"
-const app = express();
-import router from "./controllers/auth.js"
-import methodOverride from "method-override"
-import morgan from "morgan"
-import session from "express-session"
-import mongstore from "connect-mongo"
-// Set the port from environment variable or default to 3000
+import "dotenv/config";
+import express from "express";
+import mongoose from "mongoose";
+import MongoStore from "connect-mongo";
+import methodOverride from "method-override";
+import logger from "morgan";
+import session from "express-session";
+import router from "./controllers/auth.js";
+import passUserToView from "./middleware/pass-user-to-view.js";
 
+const app = express();
+
+const PORT = process.env.PORT ? process.env.PORT : "3000";
 
 mongoose.connect(process.env.MONGODB_URI);
-
 
 // Middleware to parse URL-encoded data from forms
 app.use(express.urlencoded({ extended: false }));
 // Middleware for using HTTP verbs such as PUT or DELETE
 app.use(methodOverride("_method"));
 // Morgan for logging HTTP requests
-app.use(morgan('dev'));
+app.use(logger("dev"));
+// Set View Engine to ejs templating
+app.set("view engine", "ejs");
+// Configure session based Auth
 app.use(
-    session({
-      secret: process.env.SESSION_SECRET,
-      resave: false,
-      saveUninitialized: true,
-      store: MongoStore.create({
-        mongoUrl: process.env.MONGODB_URI,
-      }),
-    })
-  );
-app.use("/auth", router);
-
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI,
+    }),
+  })
+);
+// Pass user to locals object middleware
+app.use(passUserToView);
 
 app.get("/", (req, res) => {
-    res.render("index.ejs", {
-      user: req.session.user,
-    });
+  res.render("index");
+});
+
+app.use("/auth", router);
+
+mongoose.connection.on("connected", () => {
+  console.clear();
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+  app.listen(PORT, () => {
+    console.log(`The express app is ready on port ${PORT}!`);
   });
-  app.get("/vip-lounge", (req, res) => {
-    if (req.session.user) {
-      res.send(`Welcome to the party ${req.session.user.username}.`);
-    } else {
-      res.send("Sorry, no guests allowed.");
-    }
-  });
-  
-app.listen(3000, () => {
-    mongoose.connection.on("connected", () => {
-        console.clear()
-        console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
-      });
-      
-    let port;
-if (process.env.PORT) {
-  port = process.env.PORT;
-} else {
-  port = 3000;
-}
 });
