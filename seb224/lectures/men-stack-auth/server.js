0c3836cb942035b16fbb49cf0b149444bffd0f1b
camiegray <camieg4@gmail.com>
2025-03-27 07:40:24 -0700

    Add controller

--- seb224/lectures/men-stack-auth/server.js
+++ seb224/lectures/men-stack-auth/server.js
@@ -1,20 +1,17 @@
-const dotenv = require("dotenv");
-dotenv.config();
-const express = require("express");
+import mongoose from "mongoose"
+import "dotenv/config"
+import express from "express"
+import ejs from "ejs"
 const app = express();
-
-const mongoose = require("mongoose");
-const methodOverride = require("method-override");
-const morgan = require("morgan");
+import router from "./controllers/auth.js"
+import methodOverride from "method-override"
+import morgan from "morgan"
 
 // Set the port from environment variable or default to 3000
-const port = process.env.PORT ? process.env.PORT : "3000";
+
 
 mongoose.connect(process.env.MONGODB_URI);
 
-mongoose.connection.on("connected", () => {
-  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
-});
 
 // Middleware to parse URL-encoded data from forms
 app.use(express.urlencoded({ extended: false }));
@@ -22,7 +19,23 @@ app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride("_method"));
 // Morgan for logging HTTP requests
 app.use(morgan('dev'));
+app.use("/", router);
+app.get("/", async (req, res) => {
+    res.render("index.ejs")
 
-app.listen(port, () => {
-  console.log(`The express app is ready on port ${port}!`);
+})
+
+
+app.listen(3000, () => {
+    mongoose.connection.on("connected", () => {
+        console.clear()
+        console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+      });
+      
+    let port;
+if (process.env.PORT) {
+  port = process.env.PORT;
+} else {
+  port = 3000;
+}
 });
