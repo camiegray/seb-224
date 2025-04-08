769e6ac08a1c355038ecb675cc8329f9e3cbc4d9
camiegray <camieg4@gmail.com>
2025-04-08 14:46:45 -0700

    Add lecture notes

--- seb224/lectures/react-state-management/src/App.jsx
+++ seb224/lectures/react-state-management/src/App.jsx
@@ -1,10 +1,40 @@
 // src/App.jsx
 
+import { useState } from 'react';
 import './App.css';
 
 const App = () => {
+
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  console.log('Our isDarkMode state value is:', isDarkMode);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  const [userInfo, setUserInfo] = useState({
+    firstName: 'Jane',  // Change 'Jane' to your first name.
+    lastName: 'Doe',    // Change 'Doe' to your last name.
+    hasPets: false,      // Change to true if you have pets, false if not.
+    age: 25             // Change 25 to your age.
+  });
+
+  console.log('User Info:', userInfo);
+
+  const petMessage = userInfo.hasPets ? 'have pets' : 'do not have pets';
+
   return (
-    <h1>Hello world!</h1>
+    <div className={isDarkMode ? "dark" : "light"}>
+      {/* Dark Mode Button */}
+      <button onClick={toggleDarkMode}>
+        Dark Mode? {isDarkMode ? "On" : "Off"}
+      </button>
+
+      <h1>
+        Hello, my name is {userInfo.firstName} {userInfo.lastName}, I am {userInfo.age} years old, and I {petMessage}.
+      </h1>
+    </div>
   );
 };
 
