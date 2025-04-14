486a7cbe5a0063291e64752b1a0b273ed4798eb0
camiegray <camieg4@gmail.com>
2025-04-14 11:29:07 -0700

    Complete lecture with full pokemon react data

--- seb224/lectures/react-router-dom/src/App.jsx
+++ seb224/lectures/react-router-dom/src/App.jsx
@@ -1,22 +1,32 @@
-// src/App.jsx
+import { useState } from "react";
+import { Route, Routes } from "react-router";
+import NavBar from "./components/NavBar/NavBar";
+import PokemonList from "./components/PokemonList/PokemonList";
+import PokemonDetails from "./components/PokemoDetails/PokemonDetails";
+import PokemonForm from "./components/PokemonForm/PokemonForm";
 
-import { useState } from 'react';
-import PokemonList from './components/PokemonList/PokemonList';
+import "./App.css";
 
 const initialState = [
-  { _id: 1, name: 'bulbasaur', weight: 69, height: 7 },
-  { _id: 2, name: 'ivysaur', weight: 130, height: 10 },
-  { _id: 3, name: 'venusaur', weight: 1000, height: 20 },
-  { _id: 4, name: 'charmander', weight: 85, height: 6 },
-  { _id: 5, name: 'charmeleon', weight: 190, height: 11 },
+  { _id: 1, name: "bulbasaur", weight: 69, height: 7 },
+  { _id: 2, name: "ivysaur", weight: 130, height: 10 },
+  { _id: 3, name: "venusaur", weight: 1000, height: 20 },
+  { _id: 4, name: "charmander", weight: 85, height: 6 },
+  { _id: 5, name: "charmeleon", weight: 190, height: 11 },
 ];
 
 const App = () => {
   const [pokemon, setPokemon] = useState(initialState);
+
   return (
     <>
-      <h1>Pokemon!</h1>
-      <PokemonList pokemon={pokemon} />
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<h1>Pokemon!</h1>} />
+        <Route path="/pokemon" element={<PokemonList pokemon={pokemon} />} />
+        <Route path="/pokemon/:id" element={<PokemonDetails pokemon={pokemon} />} />
+        <Route path="/pokemon/new" element={<PokemonForm pokemon={pokemon} setPokemon={setPokemon} />} />
+      </Routes>
     </>
   );
 };
