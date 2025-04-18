1fbc30ec15caf3faa1a0d0693a5955870e090f56
camiegray <camieg4@gmail.com>
2025-04-18 06:14:03 -0700

    First commit

---
+++ seb224/lectures/express-api-auth/models/user.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  hashedPassword: { type: String, required: true },
+});
+
+userSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    delete returnedObject.hashedPassword;
+  },
+});
+
+export default mongoose.model("users", userSchema);
