7e2d5d6311fd3675118602410f61f136cf0e9e6c
camiegray <camieg4@gmail.com>
2025-05-07 06:35:23 -0700

    New view add

--- seb224/lectures/django-catscollector/main_app/urls.py
+++ seb224/lectures/django-catscollector/main_app/urls.py
@@ -1,7 +1,9 @@
     # import Home view from the views file
-from .views import Home
+from .views import Home, CatList, CatDetail
 from django.urls import path
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
+    path('cats/', CatList.as_view(), name='cat-list'),
+    path('cats/<int:id>/', CatDetail.as_view(), name='cat-detail'),
 ]
--- seb224/lectures/django-catscollector/main_app/views.py
+++ seb224/lectures/django-catscollector/main_app/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework import generics
 from rest_framework.response import Response
+from .models import Cat
+from .serializers import CatSerializer  
 
 # Define the home view
 class Home(APIView):
   def get(self, request):
     content = {'message': 'Welcome to the cat-collector api home route!'}
     return Response(content)
+
+class CatList(generics.ListCreateAPIView):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+
+class CatDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer  
+    lookup_field = 'id'
